15c52c8a8900e850adac6d9e26b687c6c4300899
elkrien <elkrien@gmail.com>
2022-06-15 22:03:21 +0200

    qtile update

--- qtile/settings/widgets.py
+++ qtile/settings/widgets.py
@@ -1,6 +1,8 @@
-from libqtile import widget
+# from libqtile import backend, widget
+from qtile_extras import widget
+from qtile_extras.widget.decorations import RectDecoration
 from .theme import colors
-from libqtile import qtile
+from libqtile import qtile, backend
 import subprocess
 import os
 
@@ -17,9 +19,6 @@ def base(fg='text', bg='dark'):
 
 def separator():
     return widget.Sep(**base(), linewidth=0, padding=7)
-
-def separator_2():
-    return widget.Sep(**base(), linewidth=0, padding=1)
 
 def workspaces(): 
     return [
@@ -80,9 +79,10 @@ primary_widgets = [
 
     separator(),
 
+        # font='JetBrainsMonoMedium Nerd Font', 
     widget.WindowName(
         **base(fg='focus'),
-        format=' {name}', 
+        format=' {name}', 
         fontsize=14, 
         font='Open Sans semibold', 
         padding=5, 
@@ -92,15 +92,15 @@ primary_widgets = [
     separator(),
 
     widget.Systray(
-        background=colors['dark'], 
+        background=colors['dark'],
         padding=5, 
-        icon_size=19
+        icon_size=19,
     ),
     
     separator(),
 
     widget.TextBox(
-        **base(fg='orange'),
+        **base(fg='dark'),
         fontsize=17,
         font='Inter Nerd Font',
         text=' ',
@@ -108,6 +108,15 @@ primary_widgets = [
             'Button1': lambda: qtile.cmd_spawn("python3 " + home + "/.config/qtile/scripts/check_updates.py"),
             'Button3': lambda: qtile.cmd_spawn(myTerm + ' -e paru -Syu --skipreview')
         },
+        padding=4,
+        decorations=[
+            RectDecoration(
+                colour=colors['orange'],
+                radius=0,
+                filled=True,
+                padding_y=6,
+            )
+        ]
     ),
     widget.GenPollText(
 	    **base(),
@@ -120,7 +129,24 @@ primary_widgets = [
             'Button3': lambda: qtile.cmd_spawn(myTerm + ' -e paru -Syu --skipreview')
         },
         func=lambda: subprocess.check_output(home + "/.config/qtile/scripts/updates_count.sh").decode("utf-8").replace('\n', ''),
-        padding=4,
+        padding=10,
+        decorations=[
+            RectDecoration(
+                colour=colors['dark_light'],
+                radius=0,
+                filled=True,
+                padding_y=7,
+                padding_x=0,
+            ),
+            RectDecoration(
+                colour=colors['orange'],
+                radius=0,
+                filled=False,
+                line_width=2,
+                padding_y=7,
+                padding_x=1,
+            ),
+        ]
 	),
 
 
@@ -141,57 +167,109 @@ primary_widgets = [
     #     }
     # ),
 
-    separator_2(),
+    separator(),
 
-    widget.Battery(
-        **base(fg='green'),
-        fontsize=19,
-        font='Inter Nerd Font',
-        battery='BAT1',
-        charge_char='',
-        discharge_char='',
-        full_char='',
-        empty_char='',
-        unknown_char='',
-        show_short_text=False,
-        format=' {char}',    #{percent:2.0%}',
-        low_foreground=colors['red'],
-        low_percentage=0.1,
-        notify_below=0.2,
-        mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/batt.sh")},
-    ),
-    widget.Battery(
-        **base(),
-        fontsize=16,
-        font='Open Sans semibold',
-        battery='BAT1',
-        format='{percent:2.0%}',    #{percent:2.0%}',
-        show_short_text=False,
-        low_foreground=colors['red'],
-        low_percentage=0.2,
-        notify_below=0.1,
-        mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/batt.sh")},
-        padding=4,
-    ),
+    # widget.Battery(
+    #     **base(fg='dark'),
+    #     fontsize=19,
+    #     font='Inter Nerd Font bold',
+    #     battery='BAT1',
+    #     charge_char='',
+    #     discharge_char='',
+    #     full_char='',
+    #     empty_char='',
+    #     unknown_char='',
+    #     show_short_text=False,
+    #     format=' {char}',    #{percent:2.0%}',
+    #     low_foreground=colors['red'],
+    #     low_percentage=0.1,
+    #     notify_below=0.2,
+    #     mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/batt.sh")},
+    #     padding=2,
+    #     decorations=[
+    #         RectDecoration(
+    #             colour=colors['green'],
+    #             radius=0,
+    #             filled=True,
+    #             padding_y=6,
+    #         )
+    #     ]
+    # ),
+    # widget.Battery(
+    #     **base(),
+    #     fontsize=16,
+    #     font='Open Sans semibold',
+    #     battery='BAT1',
+    #     format='{percent:2.0%}',    #{percent:2.0%}',
+    #     show_short_text=False,
+    #     low_foreground=colors['red'],
+    #     low_percentage=0.2,
+    #     notify_below=0.1,
+    #     mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/batt.sh")},
+    #     padding=8,
+    #     decorations=[
+    #         RectDecoration(
+    #             colour=colors['dark_light'],
+    #             radius=0,
+    #             filled=True,
+    #             padding_y=7,
+    #             padding_x=0,
+    #         ),
+    #         RectDecoration(
+    #             colour=colors['green'],
+    #             radius=0,
+    #             filled=False,
+    #             line_width=2,
+    #             padding_y=7,
+    #             padding_x=1,
+    #         ),
+    #     ]
+    # ),
 
-    separator_2(),
+    # separator(),
 
-    widget.TextBox(
-        **base(fg='yellow'),
-        fontsize=18,
-        font='Inter Nerd Font',
-        text=' 盛',
-    ),
-    widget.Backlight(
-        **base(),
-        backlight_name='intel_backlight',
-        fmt='{}',
-        font='Open Sans semibold',
-        fontsize=16,
-        padding=4,
-    ),
+    # widget.TextBox(
+    #     **base(fg='dark'),
+    #     fontsize=18,
+    #     font='Inter Nerd Font',
+    #     text=' 盛',
+    #     padding=2,
+    #     decorations=[
+    #         RectDecoration(
+    #             colour=colors['yellow'],
+    #             radius=0,
+    #             filled=True,
+    #             padding_y=6,
+    #         )
+    #     ]
+    # ),
+    # widget.Backlight(
+    #     **base(),
+    #     # backlight_name='intel_backlight',
+    #     fmt='{}',
+    #     font='Open Sans semibold',
+    #     fontsize=16,
+    #     padding=8,
+    #     decorations=[
+    #         RectDecoration(
+    #             colour=colors['dark_light'],
+    #             radius=0,
+    #             filled=True,
+    #             padding_y=7,
+    #             padding_x=0,
+    #         ),
+    #         RectDecoration(
+    #             colour=colors['yellow'],
+    #             radius=0,
+    #             filled=False,
+    #             line_width=2,
+    #             padding_y=7,
+    #             padding_x=1,
+    #         ),
+    #     ]
+    # ),
 
-    separator_2(),
+    # separator(),
 
     # widget.PulseVolume(
     #     **base(fg='cyan'),
@@ -200,34 +278,69 @@ primary_widgets = [
     #     emoji=True
     # ),
     widget.TextBox(
-        **base(fg='cyan'),
+        **base(fg='dark'),
         fontsize=18,
         font='Inter Nerd Font',
-        text=' ',
+        text=' ',
+        padding=2,
+        decorations=[
+            RectDecoration(
+                colour=colors['blue'],
+                radius=0,
+                filled=True,
+                padding_y=6,
+            )
+        ]
     ),
     widget.PulseVolume(
         **base(),
         font='Open Sans semibold',
         fontsize=16,
-        padding=4,
+        padding=8,
         fmt='{}',
         mouse_callbacks = {'Button3': lambda: qtile.cmd_spawn("pavucontrol")},
+        decorations=[
+            RectDecoration(
+                colour=colors['dark_light'],
+                radius=0,
+                filled=True,
+                padding_y=7,
+                padding_x=0,
+            ),
+            RectDecoration(
+                colour=colors['blue'],
+                radius=0,
+                filled=False,
+                line_width=2,
+                padding_y=7,
+                padding_x=1,
+            ),
+        ]
     ),
     
-    separator_2(),
+    separator(),
 
     widget.Wlan(
-        **base(fg='purple'),
+        **base(fg='dark'),
         format=' 直', 
         disconnected_message=' 睊',
         fontsize=18,
         font='Inter Nerd Font',
-        interface='wlp3s0',
+        interface='wlp5s0',
         update_interval=5,
         mouse_callbacks = {
             'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/wifi.sh"),
             'Button3': lambda: qtile.cmd_spawn(myTerm + ' -e nmtui')
         },
+        padding=2,
+        decorations=[
+            RectDecoration(
+                colour=colors['green'],
+                radius=0,
+                filled=True,
+                padding_y=6,
+            )
+        ]
     ),
     widget.Wlan(
         **base(),
@@ -235,23 +348,49 @@ primary_widgets = [
         disconnected_message=' ',
         fontsize=16,
         font='Open Sans semibold',
-        interface='wlp3s0',
+        interface='wlp5s0',
         update_interval=5,
         mouse_callbacks = {
             'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/wifi.sh"),
             'Button3': lambda: qtile.cmd_spawn(myTerm + ' -e nmtui')
         },
-        padding=4,
+        padding=8,
+        decorations=[
+            RectDecoration(
+                colour=colors['dark_light'],
+                radius=0,
+                filled=True,
+                padding_y=7,
+                padding_x=0,
+            ),
+            RectDecoration(
+                colour=colors['green'],
+                radius=0,
+                filled=False,
+                line_width=2,
+                padding_y=7,
+                padding_x=1,
+            ),
+        ]
     ),
     
-    separator_2(),
+    separator(),
 
     widget.TextBox(
-        **base(fg='magenta'),
+        **base(fg='dark'),
         fontsize=18,
         font='Inter Nerd Font',
         mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/calendar.sh curr")},
         text=' ',
+        padding=2,
+        decorations=[
+            RectDecoration(
+                colour=colors['purple'],
+                radius=0,
+                filled=True,
+                padding_y=6,
+            )
+        ]
     ),
     widget.Clock(
         **base(),
@@ -259,24 +398,32 @@ primary_widgets = [
         font='Open Sans semibold',
         fontsize=16,
         mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/calendar.sh curr")},                    
-        padding=4,
+        padding=8,
+        decorations=[
+            RectDecoration(
+                colour=colors['dark_light'],
+                radius=0,
+                filled=True,
+                padding_y=7,
+                padding_x=0,
+            ),
+            RectDecoration(
+                colour=colors['purple'],
+                radius=0,
+                filled=False,
+                line_width=2,
+                padding_y=7,
+                padding_x=1,
+            ),
+        ]
     ),
     
-    separator_2(), 
-
-    widget.OpenWeather(
-        **base(),
-        font='Open Sans semibold',
-        fontsize=16,
-        location='Rzeszów,PL',
-        app_key='2b19a7c68543a30e31f14cbdb52555d6',
-        language='pl',
-        format='{icon} {temp} °{units_temperature}',
-    ),
+    # separator(), 
 
     widget.TextBox(
         **base(fg='red'),
         fontsize=18,
+        font='Inter Nerd Font',
         padding=10,
         mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/powermenu.sh")},
         text=''
@@ -311,20 +458,48 @@ secondary_widgets = [
     separator(),
     
     widget.TextBox(
-        **base(fg='magenta'),
+        **base(fg='dark'),
         fontsize=18,
         font='Inter Nerd Font',
         mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/calendar.sh curr")},
         text=' ',
+        padding=2,
+        decorations=[
+            RectDecoration(
+                colour=colors['magenta'],
+                radius=0,
+                filled=True,
+                padding_y=6,
+            )
+        ]
     ),
+    
     widget.Clock(
         **base(),
         format='%H:%M',
         font='Open Sans semibold',
         fontsize=16,
         mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(home + "/.config/qtile/scripts/calendar.sh curr")},                    
-        padding=4,
+        padding=8,
+        decorations=[
+            RectDecoration(
+                colour=colors['dark_light'],
+                radius=0,
+                filled=True,
+                padding_y=7,
+                padding_x=0,
+            ),
+            RectDecoration(
+                colour=colors['magenta'],
+                radius=0,
+                filled=False,
+                line_width=2,
+                padding_y=7,
+                padding_x=1,
+            ),
+        ]
     ),
+    
     separator(), 
     
     # widget.CurrentLayoutIcon(foreground=colors['cyan'], scale=0.4),
@@ -332,7 +507,7 @@ secondary_widgets = [
 ]
 
 widget_defaults = {
-    'font': 'JetBrainsMonoMedium Nerd Font',
+    'font': 'Inter Nerd Font',
     'fontsize': 16,
     'padding': 1,
 }
